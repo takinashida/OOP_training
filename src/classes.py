7fe34aade270779b434c91f10d3849de8c6642fe
takinashida <takinashida@gmail.com>
2025-06-21 21:09:04 +0300

    functionalities done

--- src/classes.py
+++ src/classes.py
@@ -7,8 +7,41 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        if new_price < self.__price:
+            agree = input("Цена снижается. Продолжить? (y/n): ")
+            if agree.strip().lower() != "y":
+                print("Действие было отменено")
+                return
+        self.__price = new_price
+
+
+    @classmethod
+    def new_product(cls, product_dict: dict, products: list):
+        for product in products:
+            if product_dict["name"] == product.name:
+                product.quantity += product_dict["quantity"]
+                product.price = max(product_dict["price"], product.price)
+                return product
+        return cls(
+        name=product_dict["name"],
+        description=product_dict["description"],
+        price=product_dict["price"],
+        quantity=product_dict["quantity"]
+        )
+
+
 
 
 class Category:
@@ -21,6 +54,23 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = list(products)
+        self.__products = list(products)
         self.product_count = len(products)
         self.category_count += 1
+
+
+    def add_product(self, new_product):
+        self.__products.append(new_product)
+
+
+    @property
+    def products(self):
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб., Остаток: {product.quantity} шт.")
+        return result
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from config import ROOT_DIR
+from src.classes import Category, Product
 from src.utils import get_classes, json_load
 
 if __name__ == "__main__":
@@ -6,7 +7,13 @@ if __name__ == "__main__":
 
     categories, products = get_classes(json_load(path_to_json))
 
-    print(categories, "\n", products)
     phones = categories[0]
-    for phone in phones.products:
-        print(phone.name)
+    new = []
+    for prod in phones.products:
+        new.append({"name": prod.name,"description": prod.description, "price": prod.price, "quantity": prod.quantity})
+    print(new)
+    # phone = {"name": "iphone","description": "None", "price": 150, "quantity": 1}
+    # new_1 = Product.new_product(new, phone)
+    # for phone in phones.products:
+    #     print(phone)
+
